6e8da3905b3aa96e45923d59ef1b2fb1554d90d6
Salman <salman.hashmi2@mail.mcgill.ca>
2017-09-05 01:34:24 -0400

    chore: Revert to manual option for starting IK_server node
    This allows for the node code to be modified and tested during simulation without having to close Gazebo and RViz.

--- kuka_arm/scripts/safe_spawner.sh
+++ kuka_arm/scripts/safe_spawner.sh
@@ -7,6 +7,4 @@ sleep 3 &&
 x-terminal-emulator -e roslaunch kuka_arm spawn_target.launch &
 sleep 5 &&
 #x-terminal-emulator -e roslaunch kuka_arm inverse_kinematics.launch
-roslaunch kuka_arm inverse_kinematics.launch &
-sleep 5 &&
-x-terminal-emulator -e rosrun kuka_arm IK_server.py
+roslaunch kuka_arm inverse_kinematics.launch

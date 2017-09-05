7d0135b995200331dcf7c0357647e6f2930be607
Salman <salman.hashmi2@mail.mcgill.ca>
2017-09-05 01:46:42 -0400

    feat: Compare EE positions from FK to those received in IK request
    Add functions to compare gripper or EE positions from calculated joint angles (FK) with those received in the EE pose from IK service request, and plot these positions and the overall EE error or offset to verify accuracy of IK.

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -27,6 +27,9 @@ from geometry_msgs.msg import Pose
 from numpy import array, matrix, cos, sin, pi, arccos, arctan2, sqrt
 from numpy.linalg import inv
 
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot as plt
+
 
 def get_DH_Table():
     """
@@ -171,8 +174,8 @@ def get_joints1_2_3(dh, Wc):
     # theta2,3 are calculated using Cosine Law on a triangle with edges
     # at joints 1,2 and WC viewed from side and
     # forming angles A, B and C repectively
-    wcz_j2 = wcz - dh['d1']                              # WC z-component from j2
-    wcx_j2 = sqrt(wcx**2 + wcy**2) - dh['a1']            # WC x-component from j2
+    wcz_j2 = wcz - dh['d1']                            # WC z-component from j2
+    wcx_j2 = sqrt(wcx**2 + wcy**2) - dh['a1']          # WC x-component from j2
 
     side_a = round(sqrt((dh['d4'])**2 + (dh['a3'])**2), 7)  # line segment: j3-WC
     side_b = sqrt(wcx_j2**2 + wcz_j2**2)                    # line segment: j2-WC
@@ -184,7 +187,7 @@ def get_joints1_2_3(dh, Wc):
 
     # The sag between joint-3 and WC is due to a3 and its angle is formed
     # between y3-axis and side_a
-    angle_sag = round(arctan2(abs(dh['a3']),dh['d4']), 7)
+    angle_sag = round(arctan2(abs(dh['a3']), dh['d4']), 7)
 
     theta2 = pi/2 - angleA - arctan2(wcz_j2, wcx_j2)
     theta3 = pi/2 - (angleB + angle_sag)
@@ -218,11 +221,11 @@ def get_joints4_5_6(dh, R_ee, theta1, theta2, theta3):
     # x-y-z (roll-pitch-yaw) rotation sequence that orients WC
     R3_6 = inv(array(R0_3, dtype='float')) * R_ee  # b/c R0_6 == R_ee = R0_3*R3_6
 
-    r21 = R3_6[1, 0]              # sin(theta5)*cos(theta6)
-    r22 = R3_6[1, 1]              # -sin(theta6)*sin(theta6)
-    r13 = R3_6[0, 2]              # -sin(theta5)*cos(theta4)
-    r23 = R3_6[1, 2]              # cos(theta5)
-    r33 = R3_6[2, 2]              # sin(theta4)*sin(theta5)
+    r21 = R3_6[1, 0]  # sin(theta5)*cos(theta6)
+    r22 = R3_6[1, 1]  # -sin(theta5)*sin(theta6)
+    r13 = R3_6[0, 2]  # -sin(theta5)*cos(theta4)
+    r23 = R3_6[1, 2]  # cos(theta5)
+    r33 = R3_6[2, 2]  # sin(theta4)*sin(theta5)
 
     # Compute Euler angles theta 4,5,6 from R3_6 by individually
     # isolating and explicitly solving each angle
@@ -231,6 +234,45 @@ def get_joints4_5_6(dh, R_ee, theta1, theta2, theta3):
     theta6 = arctan2(-r22, r21)
 
     return theta4, theta5, theta6
+
+
+def plot_EE(received_ee_points, fk_ee_points, ee_errors):
+    """
+    Plot and compare EE positions received in IK request with those from FK.
+
+    NOTE: Plot window for a particular requested trajectory will pop up during
+          pick-place simulation prior to the execution of that trajectory
+          and will have to be closed to unpause the pick-place operation
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    data1 = array(received_ee_points)
+    x1, y1, z1 = data1.T
+    ax.scatter(x1, y1, z1, c='blue', s=50, marker='o')
+    ax.plot(x1, y1, z1, c='blue', label='rec_ee')
+
+    data2 = array(fk_ee_points)
+    x2, y2, z2 = data2.T
+    ax.scatter(x2, y2, z2, c='orange', s=50, marker='s')
+    ax.plot(x2, y2, z2, c='orange', label='fk_ee')
+
+    data3 = array(ee_errors)
+    x3, y3, z3 = data3.T
+    ax.scatter(x3, y3, z3, c='magenta', s=50, marker='^')
+    ax.plot(x3, y3, z3, c='magenta', label='ee_error')
+
+    ax.set_xlabel('X Axis')
+    ax.set_ylabel('Y Axis')
+    ax.set_zlabel('Z Axis')
+
+    ax.xaxis.label.set_color('red')
+    ax.yaxis.label.set_color('green')
+    ax.zaxis.label.set_color('blue')
+
+    #ax.view_init(30,220)
+    plt.legend()
+    plt.show()
 
 
 def handle_calculate_IK(req):
@@ -246,6 +288,11 @@ def handle_calculate_IK(req):
         # to a given gripper pose
         joint_trajectory_list = []
 
+        # To store coordinates for plotting (in plot_ee() function)
+        received_ee_points = []
+        fk_ee_points = []
+        ee_errors = []
+
         # For each gripper pose a response of six joint angles is computed
         len_poses = len(req.poses)
         for x in xrange(0, len_poses):
@@ -253,6 +300,8 @@ def handle_calculate_IK(req):
 
             # INVERSE KINEMATICS
             ee_pose = get_ee_pose(req.poses[x])
+
+            received_ee_points.append(ee_pose[0])
 
             R_ee = get_R_EE(ee_pose)
             Wc = get_WC(dh, R_ee, ee_pose)
@@ -274,8 +323,39 @@ def handle_calculate_IK(req):
                                                 theta4, theta5, theta6]
             joint_trajectory_list.append(joint_trajectory_point)
 
+            def calculate_FK():
+                """Calculate Forward Kinematics for verifying joint angles."""
+                # Compute individual transforms between adjacent links
+                # T(i-1)_i = Rx(alpha(i-1)) * Dx(alpha(i-1)) * Rz(theta(i)) * Dz(d(i))
+                T0_1 = get_TF(dh['alpha0'], dh['a0'], dh['d1'], dh['theta1'])
+                T1_2 = get_TF(dh['alpha1'], dh['a1'], dh['d2'], dh['theta2'])
+                T2_3 = get_TF(dh['alpha2'], dh['a2'], dh['d3'], dh['theta3'])
+                T3_4 = get_TF(dh['alpha3'], dh['a3'], dh['d4'], dh['theta4'])
+                T4_5 = get_TF(dh['alpha4'], dh['a4'], dh['d5'], dh['theta5'])
+                T5_6 = get_TF(dh['alpha5'], dh['a5'], dh['d6'], dh['theta6'])
+                T6_ee = get_TF(dh['alpha6'], dh['a6'], dh['dG'], dh['thetaG'])
+                # Create overall transform between base frame and EE by
+                # composing the individual link transforms
+                T0_ee = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_ee
+                fk_ee = [T0_ee[0, 3], T0_ee[1, 3], T0_ee[2, 3]]
+                fk_ee_points.append([round(fk_ee[0].item(0), 8),
+                                     round(fk_ee[1].item(0), 8),
+                                     round(fk_ee[2].item(0), 8)])
+                ee_x_e = abs(fk_ee[0] - ee_pose[0][0])
+                ee_y_e = abs(fk_ee[1] - ee_pose[0][1])
+                ee_z_e = abs(fk_ee[2] - ee_pose[0][2])
+                ee_errors.append([round(ee_x_e.item(0), 8),
+                                  round(ee_y_e.item(0), 8),
+                                  round(ee_z_e.item(0), 8)])
+            # NOTE: Uncomment following line to compute FK for plotting EE
+            #calculate_FK()
+
         rospy.loginfo("Number of joint trajectory points:" +
                       " %s" % len(joint_trajectory_list))
+
+        # NOTE: Uncomment the following line to plot EE trajectories
+        #plot_EE(received_ee_points, fk_ee_points, ee_errors)
+
         return CalculateIKResponse(joint_trajectory_list)
 
 
